a0f1ac81f5f5b2c12de56450076f6eca159cf8da
John <john@Johns-MacBook-Pro.local>
2018-10-15 12:52:12 -0500

    commit 4: set up test route

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
-
+//db
 require('./db/db');
 
+//middleware
+
+//test route
+app.get('/', (req, res) => {
+	res.send('welcome to the game')
+});
+
+//listener
 const post = 3000;
 app.listen(post, () => {
 	console.log('server is listening at ' + post);
